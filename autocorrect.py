94e3b88c21e15a34741f920081006b65c8ac40d5
GeorgeFreidrick <50186316+GeorgeFreidrick@users.noreply.github.com>
2020-04-27 12:21:27 -0700

    Added getAutocorrect

--- autocorrect.py
+++ autocorrect.py
@@ -1,1 +1,20 @@
-# i still don't know how to breathe
+import Levenshtein
+
+def getAutocorrect(text):
+
+    # Loop over each word in the list
+    candidates = {}
+    with open('wordlist.txt') as words:
+        for word in words:
+
+            # Use the Levenshtein function to get the number of different characters
+            score = Levenshtein.jaro_winkler(text, word)
+            candidates[score] = word
+
+    # Determine the 5 words with the lowest score (closest to the word typed in)
+    autocorrected = []
+    for i in range(0, 6):
+        autocorrected.append(list(sorted(candidates.values()))[i])
+
+    # Return list of 5 words
+    return autocorrected
