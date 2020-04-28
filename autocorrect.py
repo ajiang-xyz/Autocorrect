2995cc2d1fd8c5c25ab241163d11883407eddcf1
GeorgeFreidrick <50186316+GeorgeFreidrick@users.noreply.github.com>
2020-04-28 13:00:57 -0700

    Updated functionality

--- autocorrect.py
+++ autocorrect.py
@@ -1,35 +1,55 @@
 from collections import OrderedDict 
 from fuzzywuzzy import fuzz
 
+def getKeysByValue(dictOfElements, valueToFind):
+    listOfKeys = list()
+    listOfItems = dictOfElements.items()
+    for item  in listOfItems:
+        if item[1] == valueToFind:
+            listOfKeys.append(item[0])
+    return  listOfKeys
+
 def getAutocorrect(text):
 
     # Loop over each word in the list
     candidates = {}
     with open('wordlist.txt') as words:
         for word in words:
+            word.replace("\n", "")
+            word = word.rstrip("\r\n")
 
             # Use Fuzzywuzzy to determine the percentage of "sameness"
             score = fuzz.ratio(word, text)
-            candidates[score] = word
+            candidates[word] = score
 
     # Determine the 5 words with the highest match percentage (closest to the word typed in)
 
     candidates = dict(OrderedDict(sorted(candidates.items())))
     edited = {}
-    for score in candidates.keys():
+    for score in candidates.values():
         if score >= 75:
-            edited[candidates[score]] = score
+            for key in getKeysByValue(candidates, score):
+                edited[key] = score
 
     autocorrected = []
     scores = []
-    for wordNewline in edited.keys():
-        word = wordNewline.replace("\n", "")
+    for word in edited.keys():
         autocorrected.append(word)
 
     for score in edited.values():
         scores.append(score)
 
     # Return list of 5 words
+    # autocorrected.reverse()
+    # scores.reverse()
+
+    sort = {autocorrected[i]: scores[i] for i in range(len(autocorrected))}
+    sort = dict(OrderedDict(sorted(sort.items())))
+    sort = {k: v for k, v in sorted(sort.items(), key=lambda item: item[1])}
+    autocorrected = list(sort.keys())
+    scores = list(sort.values())
+    
+
     autocorrected.reverse()
     scores.reverse()
     return autocorrected, scores
@@ -39,10 +59,10 @@ def printAutocorrected(wordsList, scores):
     words = ""
 
     for i in range(0, len(wordsList)):
-        words += wordsList[i] + "({0}) ".format(scores[i])
+        words += wordsList[i] + " ({0}%) ".format(scores[i])
     
     words = words.strip()
-    words = words.replace(" ", ", ")
+    words = words.replace(") ", "), ")
 
     print(words)
 
@@ -50,5 +70,5 @@ def printAutocorrected(wordsList, scores):
 def main():
     pass
 
-autocorrectedList, scores = getAutocorrect("apogeotropims")
-printAutocorrected(autocorrectedList, scores)
+autocorrected, scores = getAutocorrect("apogeotropizm")
+printAutocorrected(autocorrected, scores)
